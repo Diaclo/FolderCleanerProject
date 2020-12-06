2c46f679458c4aa252bf9a0565b23af324cc0ca3
alessioarcara <Spammer123@>
2020-12-06 21:20:32 +0100

    TODO: Date Management DONE: Input Format

--- FolderCleaner.py
+++ FolderCleaner.py
@@ -2,28 +2,38 @@ import os
 from datetime import datetime
 
 basedir = '/Users/alessioarcara/Downloads/Python_Script/'
-dt = str(datetime.today().strftime('%Y-%m-%d'))
 memoryList = []
 
 
 class FolderCleaner:
 
-    def renameAllFiles(self, z=0, x=0, c=0):
+    def renameAllFiles(self):
         for filename in os.listdir(basedir):
-            memoryList.append(filename.lower())
+            c = 1
+            try:
+                mtimeUnix = int(os.path.getmtime(f'{basedir}{filename}'))
+                mtimDate = datetime.fromtimestamp(mtimeUnix).strftime('%d-%m-%Y')
+            except OSError:
+                mtimDate = 0
             if 'basi' in filename.lower():
-                if ('basi' in s for s in memoryList):
-                    z += 1
-                os.replace(basedir + filename, f'{basedir}Basi_dati_{z}_{dt}.mp4')
-            elif 'ing' in filename.lower():
-                if ('ing' in s for s in memoryList):
-                    x += 1
-                os.replace(basedir + filename, f'{basedir}Ing_Software_{x}_{dt}.mp4')
-            elif 'str' in filename.lower():
-                if ('str' in s for s in memoryList):
+                filename_def = f'Basi_dati_{c}_{mtimDate}.mp4'
+                while (filename_def in memoryList):
+                    filename_def = f'Basi_dati_{c}_{mtimDate}.mp4'
                     c += 1
-                os.replace(basedir + filename, f'{basedir}Strat_Org_Mercati_{c}_{dt}.mp4')
-            print(memoryList)
+                os.replace(basedir + filename, f'{basedir}{filename_def}')
+            elif 'ing' in filename.lower():
+                filename_def = f'Ing_Software_{c}_{mtimDate}.mp4'
+                while (filename_def in memoryList):
+                    filename_def = f'Ing_Software_{c}_{mtimDate}.mp4'
+                    c += 1
+                os.replace(basedir + filename, f'{basedir}{filename_def}')
+            elif 'str' in filename.lower():
+                filename_def = f'Strat_Org_Mercati_{c}_{mtimDate}.mp4'
+                while (filename_def in memoryList):
+                    filename_def = f'Strat_Org_Mercati_{c}_{mtimDate}.mp4'
+                    c += 1
+                os.replace(basedir + filename, f'{basedir}{filename_def}')
+            memoryList.append(filename_def)
 
         return True
 
--- FolderCleanerTestCase.py
+++ FolderCleanerTestCase.py
@@ -1,6 +1,7 @@
 import unittest
 from FolderCleaner import FolderCleaner
 import os
+import time
 from datetime import datetime
 
 
@@ -9,26 +10,34 @@ class FolderCleanerTestCase(unittest.TestCase):
     dt = str(datetime.today().strftime('%Y-%m-%d'))
 
     def setUp(self):
-        files = ['basi.mp4', 'basi2.mp4', 'ing.mp4', 'ing2.mp4', 'str.mp4', 'str2.mp4']
-        for file in files:
-            with open(self.dir + file, 'a'):
-                os.utime(self.dir + file, None)
 
-    def tearDown(self):
-        for filename in os.listdir(self.dir):
-            file_path = os.path.join(self.dir, filename)
-            print(filename)
-            try:
-                os.remove(file_path)
-            except Exception as e:
-                print('Impossibile da cancellare %s. Reason %s' % (file_path, e))
+        with open('/Users/alessioarcara/Downloads/Files.txt', 'r') as f:
+            for l in f:
+                fields = l.split('; ')
+                namefile = fields[0]
+                date = datetime.strptime(fields[1].rstrip("\n"), "%d/%m/%Y")
+                # datetime into untix timestamp
+                utime = time.mktime(date.timetuple())
+
+                # os.utime(path, (atime, mtime)): atime, mtime: untix timestamp
+                with open(self.dir + namefile, 'a'):
+                    os.utime(self.dir + namefile, (utime, utime))
+
+    # def tearDown(self):
+    #     for filename in os.listdir(self.dir):
+    #         file_path = os.path.join(self.dir, filename)
+    #         print(filename)
+    #         try:
+    #             os.remove(file_path)
+    #         except Exception as e:
+    #             print('Impossibile da cancellare %s. Reason %s' % (file_path, e))
 
     def testRenameMultipleFiles(self):
-        files = [f'{self.dir}Basi_dati_{1}_{self.dt}.mp4',
-                 f'{self.dir}Basi_dati_{2}_{self.dt}.mp4',
-                 f'{self.dir}Ing_Software_{1}_{self.dt}.mp4',
-                 f'{self.dir}Ing_Software_{2}_{self.dt}.mp4',
-                 f'{self.dir}Strat_Org_Mercati_{1}_{self.dt}.mp4',
-                 f'{self.dir}Strat_Org_Mercati_{2}_{self.dt}.mp4']
+        files = [f'{self.dir}Basi_dati_{1}_20-11-2020.mp4',
+                 f'{self.dir}Basi_dati_{1}_21-11-2020.mp4',
+                 f'{self.dir}Ing_Software_{1}_22-10-2020.mp4',
+                 f'{self.dir}Ing_Software_{2}_22-10-2020.mp4',
+                 f'{self.dir}Strat_Org_Mercati_{1}_23-12-1998.mp4',
+                 f'{self.dir}Strat_Org_Mercati_{1}_24-12-1998.mp4']
         self.assertEqual(FolderCleaner.renameAllFiles(self), os.path.isfile(
             files[0] and files[1] and files[2] and files[3] and files[4] and files[5]))

06d3b7bc6879ba6c1d6648a5f5969ee22b854911
alessioarcara <Spammer123@>
2020-12-07 23:35:13 +0100

    DONE: Refactoring RenameFiles, MoveFiles TODO: DeleteFiles

--- FolderCleaner.py
+++ FolderCleaner.py
@@ -1,43 +1,49 @@
 import os
 from datetime import datetime
+import shutil
 
+extensions = ['flv', 'mkv', 'mp4']
 basedir = '/Users/alessioarcara/Downloads/Python_Script/'
+enddir = '/Users/alessioarcara/Downloads/End_Dir/'
 memoryList = []
 
 
 class FolderCleaner:
 
-    def renameAllFiles(self):
-        for filename in os.listdir(basedir):
+    @staticmethod
+    def renameFiles(base_dir, nome, nome_def):
+        for filename in os.listdir(base_dir):
             c = 1
             try:
-                mtimeUnix = int(os.path.getmtime(f'{basedir}{filename}'))
+                mtimeUnix = int(os.path.getmtime(f'{base_dir}{filename}'))
                 mtimDate = datetime.fromtimestamp(mtimeUnix).strftime('%d-%m-%Y')
             except OSError:
                 mtimDate = 0
-            if 'basi' in filename.lower():
-                filename_def = f'Basi_dati_{c}_{mtimDate}.mp4'
-                while (filename_def in memoryList):
-                    filename_def = f'Basi_dati_{c}_{mtimDate}.mp4'
+            if nome in filename.lower() and filename.lower().endswith(tuple(extensions)):
+                filename_def = f'{nome_def}_{c}_{mtimDate}.mp4'
+                while filename_def in memoryList:
+                    filename_def = f'{nome_def}_{c}_{mtimDate}.mp4'
                     c += 1
-                os.replace(basedir + filename, f'{basedir}{filename_def}')
-            elif 'ing' in filename.lower():
-                filename_def = f'Ing_Software_{c}_{mtimDate}.mp4'
-                while (filename_def in memoryList):
-                    filename_def = f'Ing_Software_{c}_{mtimDate}.mp4'
-                    c += 1
-                os.replace(basedir + filename, f'{basedir}{filename_def}')
-            elif 'str' in filename.lower():
-                filename_def = f'Strat_Org_Mercati_{c}_{mtimDate}.mp4'
-                while (filename_def in memoryList):
-                    filename_def = f'Strat_Org_Mercati_{c}_{mtimDate}.mp4'
-                    c += 1
-                os.replace(basedir + filename, f'{basedir}{filename_def}')
-            memoryList.append(filename_def)
-
+                os.replace(os.path.join(base_dir, filename), os.path.join(base_dir, filename_def))
+                memoryList.append(filename_def)
         return True
 
+    @staticmethod
+    def moveFiles(base_dir, end_dir, nome):
+        for filename in os.listdir(base_dir):
+            if nome in filename.lower():
+                shutil.move(os.path.join(base_dir, filename), os.path.join(end_dir, filename))
+        return True
+
+    @staticmethod
+    def deleteFiles(base_dir):
+        for filename in os.listdir(base_dir):
+            if not filename.endswith(tuple(extensions)):
+                os.remove(os.path.join(base_dir, filename))
+        return False
 
 if __name__ == '__main__':
     folderCleaner = FolderCleaner()
-    folderCleaner.renameAllFiles()
+    folderCleaner.renameFiles('basi', 'Basi_dati')
+    folderCleaner.renameFiles('ing', 'Ing_Software')
+    folderCleaner.renameFiles('str', 'Strat_Org_Mercati')
--- FolderCleanerTestCase.py
+++ FolderCleanerTestCase.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 
 class FolderCleanerTestCase(unittest.TestCase):
-    dir = '/Users/alessioarcara/Downloads/Python_Script/'
-    dt = str(datetime.today().strftime('%Y-%m-%d'))
+    base_dir = '/Users/alessioarcara/Downloads/Python_Script/'
+    end_dir = '/Users/alessioarcara/Downloads/End_Dir/'
 
     def setUp(self):
 
@@ -20,24 +20,32 @@ class FolderCleanerTestCase(unittest.TestCase):
                 utime = time.mktime(date.timetuple())
 
                 # os.utime(path, (atime, mtime)): atime, mtime: untix timestamp
-                with open(self.dir + namefile, 'a'):
-                    os.utime(self.dir + namefile, (utime, utime))
+                with open(self.base_dir + namefile, 'a'):
+                    os.utime(self.base_dir + namefile, (utime, utime))
 
-    # def tearDown(self):
-    #     for filename in os.listdir(self.dir):
-    #         file_path = os.path.join(self.dir, filename)
-    #         print(filename)
-    #         try:
-    #             os.remove(file_path)
-    #         except Exception as e:
-    #             print('Impossibile da cancellare %s. Reason %s' % (file_path, e))
+    def tearDown(self):
+        for filename in os.listdir(self.base_dir):
+            file_path = os.path.join(self.base_dir, filename)
+            print(filename)
+            try:
+                os.remove(file_path)
+            except Exception as e:
+                print('Impossibile da cancellare %s. Reason %s' % (file_path, e))
 
-    def testRenameMultipleFiles(self):
-        files = [f'{self.dir}Basi_dati_{1}_20-11-2020.mp4',
-                 f'{self.dir}Basi_dati_{1}_21-11-2020.mp4',
-                 f'{self.dir}Ing_Software_{1}_22-10-2020.mp4',
-                 f'{self.dir}Ing_Software_{2}_22-10-2020.mp4',
-                 f'{self.dir}Strat_Org_Mercati_{1}_23-12-1998.mp4',
-                 f'{self.dir}Strat_Org_Mercati_{1}_24-12-1998.mp4']
-        self.assertEqual(FolderCleaner.renameAllFiles(self), os.path.isfile(
-            files[0] and files[1] and files[2] and files[3] and files[4] and files[5]))
+    def testRenameFiles(self):
+        files = [f'{self.base_dir}Basi_dati_{1}_20-11-2020.mp4',
+                 f'{self.base_dir}Basi_dati_{1}_21-11-2020.mp4']
+        self.assertEqual(FolderCleaner.renameFiles(self.base_dir, 'basi', 'Basi_dati'), os.path.isfile(files[0] and files[1]))
+
+    def testMoveFiles(self):
+        files = [f'{self.end_dir}Basi.mp4',
+                 f'{self.end_dir}Basi2.mp4',
+                 f'{self.end_dir}Basi3.mp4']
+        self.assertEqual(FolderCleaner.moveFiles(self.base_dir, self.end_dir, 'basi'), os.path.isfile(files[0] and files[1]))
+
+    def testDeleteFiles(self):
+        files = [f'{self.base_dir}Garbage',
+                 f'{self.base_dir}Garbage2',
+                 f'{self.base_dir}Garbage3',
+                 f'{self.base_dir}Garbage4']
+        self.assertEqual(FolderCleaner.deleteFiles(self.base_dir), os.path.isfile(files[0] and files[1] and files[2] and files[3]))
